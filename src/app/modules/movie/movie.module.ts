3b11718dd73470479c2dddd2f8c22fef8cf25e9f
irobaina <irobaina@tritansoftware.com>
2019-11-21 17:20:18 -0500

    fix movie module

--- src/app/modules/movie/movie.module.ts
+++ src/app/modules/movie/movie.module.ts
@@ -2,20 +2,21 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { MovieRoutingModule } from "./movie-routing.module";
-import { MovieComponent } from "../../components";
+import { MovieComponent, FilmDetailsComponent } from "../../components";
 import { MovieService } from "../../service";
 import { SharedMaterialModule } from "../shared.material.module";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { TruncatePipe } from "src/app/pipe";
+import { DetailsFilmGuard } from "src/app/resolves/details-film.guard";
 
 @NgModule({
-  declarations: [MovieComponent, TruncatePipe],
+  declarations: [MovieComponent, TruncatePipe, FilmDetailsComponent],
   imports: [
     CommonModule,
     MovieRoutingModule,
     SharedMaterialModule,
     FlexLayoutModule
   ],
-  providers: [MovieService]
+  providers: [MovieService, DetailsFilmGuard]
 })
 export class MovieModule {}
